08c5e7656b12e30b34cf0d2e52f343b93d5a981c
Luan Tenorio <lluantenorio7@gmail.com>
2026-01-22 20:58:15 -0300

    Init RBT

---
+++ tree/redblack/interface.go
@@ -0,0 +1,19 @@
+package redblack
+
+const (
+	red uint8 = iota
+	black
+)
+
+type treeNode struct {
+	Left   *treeNode
+	Right  *treeNode
+	Father *treeNode
+	color  uint8
+	Key    int
+	Data   interface{}
+}
+
+type RedBlackTree struct {
+	Root *treeNode
+}

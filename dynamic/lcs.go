29a3a05268f3186912808357b6f665095097654d
Luan Tenorio <lluantenorio7@gmail.com>
2026-02-08 00:44:13 -0300

    Print LCS

--- dynamic/lcs.go
+++ dynamic/lcs.go
@@ -1,6 +1,8 @@
 package dynamic
 
-func LcsLength(X string, Y string) ([][]int, [][]rune) {
+import "fmt"
+
+func LcsLength(X, Y string) ([][]int, [][]rune) {
 	m, n := len(X), len(Y)
 	c, b := make([][]int, m+1), make([][]rune, m+1)
 
@@ -24,4 +26,53 @@ func LcsLength(X string, Y string) ([][]int, [][]rune) {
 	}
 
 	return c, b
+}
+
+func PrintLcs(table [][]rune, X string, i, j int) {
+	if i == 0 || j == 0 {
+		return
+	}
+
+	switch table[i][j] {
+	case '↖':
+		PrintLcs(table, X, i-1, j-1)
+		fmt.Printf("%c", X[i-1])
+	case '←':
+		PrintLcs(table, X, i, j-1)
+	default:
+		PrintLcs(table, X, i-1, j)
+	}
+
+	if i == len(X) {
+		fmt.Println()
+	}
+}
+
+func PrintArrowsTable(b [][]rune, X, Y string) {
+	fmt.Print("      ∅ ")
+
+	for _, char := range Y {
+		fmt.Printf(" %c ", char)
+	}
+
+	fmt.Println()
+
+	for i := 0; i < len(b); i++ {
+		if i == 0 {
+			fmt.Print(" ∅ | ")
+		} else {
+			fmt.Printf(" %c | ", X[i-1])
+		}
+
+		for j := 0; j < len(b[i]); j++ {
+			val := b[i][j]
+			if val == 0 {
+				fmt.Print(" . ")
+			} else {
+				fmt.Printf(" %c ", val)
+			}
+		}
+
+		fmt.Println()
+	}
 }
